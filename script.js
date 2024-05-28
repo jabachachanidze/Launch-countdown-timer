6ff27a3bfaa680b2746ef1bd5fc81607422bf47c
jaba chachanidze <you@example.comgeorgianjaba@gmail.com>
2024-05-28 17:14:35 +0400

    change date at script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-let countDownDate = new Date("16 May, 2024 15:09:00").getTime();
+let countDownDate = new Date("31 december, 2024 15:09:00").getTime();
 console.log(countDownDate);
 // Update the count down every 1 second
 let x = setInterval(function () {
