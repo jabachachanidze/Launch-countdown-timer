548f51a3371d08efb9b16c82ac44280a5e065195
jaba chachanidze <you@example.comgeorgianjaba@gmail.com>
2024-05-28 16:33:11 +0400

    initial setup

---
+++ script.js
@@ -0,0 +1,35 @@
+// Set the date we're counting down to
+let countDownDate = new Date("16 May, 2024 15:09:00").getTime();
+console.log(countDownDate);
+// Update the count down every 1 second
+let x = setInterval(function () {
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  console.log(days, hours, minutes, seconds);
+  // Display the result in the elements with corresponding ids
+  document.getElementById("days").innerHTML = days.toString().padStart(2, "0");
+  document.getElementById("hours").innerHTML = hours
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("minutes").innerHTML = minutes
+    .toString()
+    .padStart(2, "0");
+  document.getElementById("seconds").innerHTML = seconds
+    .toString()
+    .padStart(2, "0");
+
+  // If the count down is finished, write some text
+  if (Math.floor(distance) <= 0) {
+    clearInterval(x);
+    document.querySelector(".countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
